874591da5a7e192d3d9c71761d3a0f675456534e
yacel100 <master.yac@gmail.com>
2014-08-18 16:39:56 -0400

    dia 6 - Diapostivas

--- material/tienda.sql
+++ material/tienda.sql
@@ -5,154 +5,211 @@
  Source Server Type    : MySQL
  Source Server Version : 50531
  Source Host           : localhost
- Source Database       : tienda2
+ Source Database       : yii-curso
 
  Target Server Type    : MySQL
  Target Server Version : 50531
  File Encoding         : utf-8
 
- Date: 08/12/2014 16:17:26 PM
+ Date: 08/18/2014 12:16:12 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `CARGO`
+--  Table structure for `cargo`
 -- ----------------------------
-DROP TABLE IF EXISTS `CARGO`;
-CREATE TABLE `CARGO` (
-  `COD_CARGO` int(11) NOT NULL AUTO_INCREMENT,
-  `NOMBRE_CARGO` varchar(150) DEFAULT NULL,
-  `DESC_CARGO` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`COD_CARGO`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `cargo`;
+CREATE TABLE `cargo` (
+  `id_cargo` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_cargo` varchar(45) NOT NULL,
+  `detalle_cargo` varchar(45) DEFAULT NULL,
+  `sueldo_cargo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_cargo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `CLIENTE`
+--  Table structure for `cliente`
 -- ----------------------------
-DROP TABLE IF EXISTS `CLIENTE`;
-CREATE TABLE `CLIENTE` (
-  `COD_CLIENTE` int(11) NOT NULL AUTO_INCREMENT,
-  `NOMBRE_CLIENTE` varchar(100) DEFAULT NULL,
-  `NIT_CLIENTE` int(11) DEFAULT NULL,
-  `TELF_CLIENTE` int(11) DEFAULT NULL,
-  `TIPO_CLIENTE` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`COD_CLIENTE`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_cliente` varchar(45) NOT NULL,
+  `direccion_cliente` varchar(45) DEFAULT NULL,
+  `telefono_cliente` varchar(45) DEFAULT NULL,
+  `id_tipocliente` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_cliente`),
+  KEY `id_tipocliente_idx` (`id_tipocliente`),
+  CONSTRAINT `id_tipocliente` FOREIGN KEY (`id_tipocliente`) REFERENCES `tipo_cliente` (`id_tipocliente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `CLIENTE`
+--  Table structure for `detalle_venta`
 -- ----------------------------
-BEGIN;
-INSERT INTO `CLIENTE` VALUES ('3', 'Juan Carlos', '6479332', '70783358', 'A'), ('4', 'Jorge Medrano', '3729773', '79773181', 'A'), ('5', 'marco antezana', '6369214', '79976352', 'A'), ('6', 'Juan Carlos', '5575744', '70987623', 'A'), ('7', 'Juan Carlos', '6456332', '7093947', 'A');
-COMMIT;
+DROP TABLE IF EXISTS `detalle_venta`;
+CREATE TABLE `detalle_venta` (
+  `cantidad_detalleVenta` int(11) NOT NULL,
+  `percio_detalleVenta` int(11) NOT NULL,
+  `id_venta` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  KEY `id_producto_idx` (`id_producto`),
+  KEY `id_venta_idx` (`id_venta`),
+  CONSTRAINT `id_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_venta` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `DEVOLUCION`
+--  Table structure for `empleado`
 -- ----------------------------
-DROP TABLE IF EXISTS `DEVOLUCION`;
-CREATE TABLE `DEVOLUCION` (
-  `COD_PROD` int(11) DEFAULT NULL,
-  `COD_PROV` int(11) DEFAULT NULL,
-  `FECHA_DEV` varchar(100) DEFAULT NULL,
-  `CANTIDAD_DEV` int(11) DEFAULT NULL,
-  `DESC_DEV` varchar(250) DEFAULT NULL,
-  KEY `FK_RELATIONSHIP_7` (`COD_PROD`),
-  KEY `FK_RELATIONSHIP_8` (`COD_PROV`),
-  CONSTRAINT `FK_RELATIONSHIP_7` FOREIGN KEY (`COD_PROD`) REFERENCES `PRODUCTO` (`COD_PROD`),
-  CONSTRAINT `FK_RELATIONSHIP_8` FOREIGN KEY (`COD_PROV`) REFERENCES `PROVEDDOR` (`COD_PROV`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `empleado`;
+CREATE TABLE `empleado` (
+  `id_empleado` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_empleado` varchar(45) NOT NULL,
+  `direccion_empleado` varchar(45) DEFAULT NULL,
+  `telefono_empleado` int(11) DEFAULT NULL,
+  `email_empleado` varchar(45) DEFAULT NULL,
+  `sexo_empleado` varchar(45) NOT NULL,
+  `id_cargo` int(11) NOT NULL,
+  PRIMARY KEY (`id_empleado`),
+  KEY `id_cargo_idx` (`id_cargo`),
+  CONSTRAINT `id_cargo` FOREIGN KEY (`id_cargo`) REFERENCES `cargo` (`id_cargo`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `EMPLEADO`
+--  Table structure for `empresa`
 -- ----------------------------
-DROP TABLE IF EXISTS `EMPLEADO`;
-CREATE TABLE `EMPLEADO` (
-  `COD_EMP` int(11) NOT NULL AUTO_INCREMENT,
-  `COD_CARGO` int(11) DEFAULT NULL,
-  `NOMBRE_EMP` varchar(100) DEFAULT NULL,
-  `CI_EMP` int(11) DEFAULT NULL,
-  `DIR_EMP` varchar(150) DEFAULT NULL,
-  `TELF_EMP` int(11) DEFAULT NULL,
-  `user` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`COD_EMP`),
-  KEY `FK_RELATIONSHIP_4` (`COD_CARGO`),
-  CONSTRAINT `FK_RELATIONSHIP_4` FOREIGN KEY (`COD_CARGO`) REFERENCES `CARGO` (`COD_CARGO`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `empresa`;
+CREATE TABLE `empresa` (
+  `id_empresa` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_empresa` varchar(45) DEFAULT NULL,
+  `descripcion_empresa` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_empresa`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `PEDIDO`
+--  Table structure for `pedido`
 -- ----------------------------
-DROP TABLE IF EXISTS `PEDIDO`;
-CREATE TABLE `PEDIDO` (
-  `COD_PROD` int(11) DEFAULT NULL,
-  `COD_PROV` int(11) DEFAULT NULL,
-  `FECHA_PEDIDO` varchar(100) DEFAULT NULL,
-  `CANTIDAD_PEDIDO` int(11) DEFAULT NULL,
-  `DETALLE_PEDIDO` varchar(200) DEFAULT NULL,
-  `FECHA_LLEGADA` varchar(200) DEFAULT NULL,
-  KEY `FK_RELATIONSHIP_5` (`COD_PROD`),
-  KEY `FK_RELATIONSHIP_9` (`COD_PROV`),
-  CONSTRAINT `FK_RELATIONSHIP_5` FOREIGN KEY (`COD_PROD`) REFERENCES `PRODUCTO` (`COD_PROD`),
-  CONSTRAINT `FK_RELATIONSHIP_9` FOREIGN KEY (`COD_PROV`) REFERENCES `PROVEDDOR` (`COD_PROV`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `pedido`;
+CREATE TABLE `pedido` (
+  `id_pedido` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha_solicitado` varchar(45) DEFAULT NULL,
+  `fecha_entregado` varchar(45) DEFAULT NULL,
+  `fecha_requerido` varchar(45) DEFAULT NULL,
+  `fecha_anulado` varchar(45) DEFAULT NULL,
+  `costo_pedido` varchar(45) DEFAULT NULL,
+  `estado_pedido` varchar(45) DEFAULT NULL,
+  `id_proveedor` int(11) NOT NULL,
+  PRIMARY KEY (`id_pedido`),
+  KEY `id_proveedor_idx` (`id_proveedor`),
+  CONSTRAINT `id_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `PRODUCTO`
+--  Table structure for `producto`
 -- ----------------------------
-DROP TABLE IF EXISTS `PRODUCTO`;
-CREATE TABLE `PRODUCTO` (
-  `COD_PROD` int(11) NOT NULL AUTO_INCREMENT,
-  `NOMBRE_PROD` varchar(100) DEFAULT NULL,
-  `EMPRESA_PROD` varchar(100) DEFAULT NULL,
-  `DESCRIPCION_PROD` varchar(255) DEFAULT NULL,
-  `PRECIO_PROD` int(11) DEFAULT NULL,
-  `FECHAVENC_PROD` varchar(100) DEFAULT NULL,
-  `cod_bar` int(11) DEFAULT NULL,
-  PRIMARY KEY (`COD_PROD`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `producto`;
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_producto` varchar(45) NOT NULL,
+  `codigo_producto` varchar(45) NOT NULL,
+  `stock_producto` varchar(45) DEFAULT NULL,
+  `precio_producto` varchar(45) DEFAULT NULL,
+  `id_empresa` int(11) DEFAULT NULL,
+  `id_tipoproducto` int(11) NOT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `id_empresa_idx` (`id_empresa`),
+  KEY `id_tipoproducto_idx` (`id_tipoproducto`),
+  CONSTRAINT `id_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_tipoproducto` FOREIGN KEY (`id_tipoproducto`) REFERENCES `tipo_producto` (`id_tipoproducto`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `PRODUCTO`
+--  Table structure for `proveedor`
 -- ----------------------------
-BEGIN;
-INSERT INTO `PRODUCTO` VALUES ('6', 'Leche en Polvo Instantanea', 'pil andina', 'leche entera de 2.8 kilos', '128', '12/dic/2015', '234345');
-COMMIT;
+DROP TABLE IF EXISTS `proveedor`;
+CREATE TABLE `proveedor` (
+  `id_proveedor` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_proveedor` varchar(45) DEFAULT NULL,
+  `telefono_proveedor` int(11) DEFAULT NULL,
+  `direccion_proveedor` varchar(45) DEFAULT NULL,
+  `email_proveedor` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_proveedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `PROVEDDOR`
+--  Table structure for `rol`
 -- ----------------------------
-DROP TABLE IF EXISTS `PROVEDDOR`;
-CREATE TABLE `PROVEDDOR` (
-  `COD_PROV` int(11) NOT NULL AUTO_INCREMENT,
-  `NOMBRE_PROV` varchar(100) DEFAULT NULL,
-  `ENCARGADO_PROV` varchar(100) DEFAULT NULL,
-  `TELF_PROV` int(11) DEFAULT NULL,
-  `DIR_PROV` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`COD_PROV`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `rol`;
+CREATE TABLE `rol` (
+  `id_rol` int(11) NOT NULL,
+  `nombre_rol` varchar(45) DEFAULT NULL,
+  `activo_rol` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `VENTA`
+--  Table structure for `tipo_cliente`
 -- ----------------------------
-DROP TABLE IF EXISTS `VENTA`;
-CREATE TABLE `VENTA` (
-  `COD_VENTA` int(11) NOT NULL AUTO_INCREMENT,
-  `COD_EMP` int(11) DEFAULT NULL,
-  `COD_PROD` int(11) DEFAULT NULL,
-  `COD_CLIENTE` int(11) DEFAULT NULL,
-  `FECHA_VENTA` varchar(100) DEFAULT NULL,
-  `CANTIDAD_VENTA` int(11) DEFAULT NULL,
-  `DESC_VENTA` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`COD_VENTA`),
-  KEY `FK_RELATIONSHIP_1` (`COD_CLIENTE`),
-  KEY `FK_RELATIONSHIP_2` (`COD_PROD`),
-  KEY `FK_RELATIONSHIP_3` (`COD_EMP`),
-  CONSTRAINT `FK_RELATIONSHIP_1` FOREIGN KEY (`COD_CLIENTE`) REFERENCES `CLIENTE` (`COD_CLIENTE`),
-  CONSTRAINT `FK_RELATIONSHIP_2` FOREIGN KEY (`COD_PROD`) REFERENCES `PRODUCTO` (`COD_PROD`),
-  CONSTRAINT `FK_RELATIONSHIP_3` FOREIGN KEY (`COD_EMP`) REFERENCES `EMPLEADO` (`COD_EMP`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `tipo_cliente`;
+CREATE TABLE `tipo_cliente` (
+  `id_tipocliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_tipocliente` varchar(45) DEFAULT NULL,
+  `detalle_tipocliente` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_tipocliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `tipo_producto`
+-- ----------------------------
+DROP TABLE IF EXISTS `tipo_producto`;
+CREATE TABLE `tipo_producto` (
+  `id_tipoproducto` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_tipoproducto` varchar(45) NOT NULL,
+  `detalle_tipoproducto` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_tipoproducto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `usuario`
+-- ----------------------------
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(45) DEFAULT NULL,
+  `password_usuario` varchar(45) DEFAULT NULL,
+  `activo_usuario` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `usuario_rol`
+-- ----------------------------
+DROP TABLE IF EXISTS `usuario_rol`;
+CREATE TABLE `usuario_rol` (
+  `id_usu` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  KEY `id_usuario_idx` (`id_usu`),
+  KEY `id_rol_idx` (`id_rol`),
+  CONSTRAINT `id_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_usuario` FOREIGN KEY (`id_usu`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `venta`
+-- ----------------------------
+DROP TABLE IF EXISTS `venta`;
+CREATE TABLE `venta` (
+  `id_venta` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha_venta` date DEFAULT NULL,
+  `hora_venta` timestamp NULL DEFAULT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  PRIMARY KEY (`id_venta`),
+  KEY `id_cliente_idx` (`id_cliente`),
+  KEY `id_empleado_idx` (`id_empleado`),
+  CONSTRAINT `id_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_empleado` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
